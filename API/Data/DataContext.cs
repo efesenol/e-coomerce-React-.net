ce66c951b87d0183863eafd08f25fa0cb1e24598
efesenol <efeesenol4471@gmail.com>
2025-06-10 18:18:41 +0300

    api oluşturulması

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,30 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+
+public class DataContext : DbContext
+{
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product>
+            {
+                new Product{Id=1, Name="IPhone 15", Description="Telefon açıklaması",ImageUrl="1.jpg", Price=70000, IsActive=true, Stock=100 },
+                new Product{Id=2, Name="IPhone 15 Pro", Description="Telefon açıklaması",ImageUrl="1.jpg", Price=80000, IsActive=true, Stock=100 },
+                new Product{Id=3, Name="IPhone 16", Description="Telefon açıklaması",ImageUrl="1.jpg", Price=90000, IsActive=true, Stock=100 },
+                new Product{Id=4, Name="IPhone 16 Pro", Description="Telefon açıklaması",ImageUrl="1.jpg", Price=100000, IsActive=true, Stock=100 }
+            }
+        );
+    }
+}
